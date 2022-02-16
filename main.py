aae6f07e8787456c839dbb3aabf893cc5c815800
Marcus Mortensen Jensen <marc7075@zbc.dk>
2022-02-16 10:33:09 +0100

    Added colors

--- main.py
+++ main.py
@@ -6,6 +6,18 @@ credentials = hlapi.CommunityData('GRUPPE1')
 engine = hlapi.SnmpEngine()
 context = hlapi.ContextData()
 
+# This is just an "enum" for colors in the console
+class color:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
 def get(target, oids, credentials, engine=engine, context=context, port=161):
     handler = hlapi.getCmd(
         engine,
