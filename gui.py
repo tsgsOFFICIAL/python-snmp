22e3d8e7fc395f4d1370e1e7083c0777dc11c7fa
Marcus Mortensen Jensen <marc7075@zbc.dk>
2022-02-17 10:01:05 +0100

    Divided functions into files

--- gui.py
+++ gui.py
@@ -1,52 +1,32 @@
 import tkinter as tk
+import snmp
 
-def open_gui():
+def toggle_state(oid, state):
+    def wrapper(oid=oid, state=state):
+        if state == 1:
+            print("UP")
+        else:
+            print("DOWN")
+    return wrapper
+
+def open_gui(dict_list):
     window = tk.Tk()
     window.title("Interfaces and their status")
     window.iconbitmap("logo.ico")
 
     for i in range(3):
-        for j in range(3):
-            frame = tk.Frame(
-                master=window,
-                relief=tk.RAISED,
-                borderwidth=1
+        window.rowconfigure(i, weight=1, minsize=25)
+        
+        for j in range(12):
+            window.columnconfigure(j, weight=1, minsize=25)
+            
+            btn = tk.Button(
+                text=f"Row {i}\nColumn {j}",
+                bg="green",
+                fg="white",
+                command=toggle_state(0, i)
             )
-            frame.grid(row=i, column=j)
-            label = tk.Label(master=frame, text=f"Row {i}\nColumn {j}")
-            label.pack()
+
+            btn.grid(row=i, column=j, padx=5, pady=5)
 
     window.mainloop()
-# window = tk.Tk()
-# label = tk.Label(
-#     text="Interfaces and their status",
-#     fg="white",
-#     bg="black",
-#     width=175,
-#     height=2
-# )
-# label.pack()
-
-# button = tk.Button(
-#     text="Click me!",
-#     width=25,
-#     height=5,
-#     bg="blue",
-#     fg="yellow"
-# )
-# button.pack(side=tk.LEFT)
-
-
-# window.mainloop()
-
-# print ("Both a and b are equal" if a == b else "a is greater than b" if a > b else "b is greater than a")
-
-
-# while True:
-#     # print(get(target, [target_oid], credentials)[target_oid]) # Simple method to do this
-
-#     result = get(target, [target_oid], credentials)
-#     for key in result:
-#         print(result[key])
-#     time.sleep(1)
-
